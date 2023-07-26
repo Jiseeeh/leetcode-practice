4e7db36108d40c2ebe87b265dc5a1a8c3df1d9f0
Jiseeeh <johncarlo.camara1@gmail.com>
2023-07-26 21:12:03 +0800

    .

---
+++ src/javascript/maxVowels.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function (s, k) {
+  let maxVowels = 0;
+
+  for (let i = 0; i < k; i++) {
+    if (isVowel(s[i])) maxVowels++;
+  }
+  let windowVowel = maxVowels;
+
+  for (let end = k; end < s.length; end++) {
+    let start = end - k;
+
+    if (isVowel(s[end])) {
+      windowVowel++;
+    }
+
+    if (isVowel(s[start])) {
+      windowVowel--;
+    }
+
+    maxVowels = Math.max(maxVowels, windowVowel);
+  }
+
+  return maxVowels;
+};
+
+const isVowel = (char) => {
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    return true;
+  }
+  return false;
+};
+
+console.log(maxVowels("abciiidef", 3));
+console.log(maxVowels("leetcode", 3));
